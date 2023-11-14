3ab9a0b157755c40c384411af4da3bb8393b9fa7
danielasakpa <danielasakpa@gmail.com>
2023-11-14 10:02:18 +0100

    add a new PORT

--- proxy-server.js
+++ proxy-server.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const cors = require('cors'); // Import cors module
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 app.use(cors()); // Use cors middleware
 app.use(express.json());
