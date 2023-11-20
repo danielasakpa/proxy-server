c194f5e5602b33567717a7cb236728daf9e11a13
danielasakpa <danielasakpa@gmail.com>
2023-11-20 12:43:26 +0100

    Add memory cache using 'memory-cache' middleware and enable compression with 'compression' middleware

---
+++ server - Copy.js
@@ -0,0 +1,110 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors'); // Import cors module
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+
+const allowedOrigins = [
+    'https://manga-website1.netlify.app',
+    'http://localhost:3000',
+];
+
+app.use(cors({
+    origin: allowedOrigins,
+}));
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// API proxy route
+
+app.get('/api', async (req, res) => {
+    try {
+        const apiUrl = req.query.url;
+
+        const response = await axios.get(apiUrl, { withCredentials: false, });
+
+        res.json(response.data);
+    } catch (error) {
+        console.error('API Proxy Error:', error.message);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+app.get('/manga', async (req, res) => {
+    try {
+        const apiUrl = req.query.url;
+
+        const response = await axios.get(apiUrl, { withCredentials: false, params: { title: req.query.title } });
+
+        res.json(response.data);
+    } catch (error) {
+        console.error('API Proxy Error:', error.message);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+
+app.get('/mangas', async (req, res) => {
+    try {
+        const apiUrl = req.query.url;
+
+        console.log(req.query.url)
+
+        const response = await axios.get(apiUrl,
+            {
+                withCredentials: false,
+                params: {
+                    includedTags: req.query.includedTags,
+                    excludedTags: req.query.excludedTags,
+                    order: req.query.order,
+                    limit: req.query.limit
+                },
+                maxContentLength: Infinity,
+            });
+        res.json(response.data);
+    } catch (error) {
+        console.error('API Proxy Error:', error.message);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+app.get('/chapters', async (req, res) => {
+    try {
+        const apiUrl = req.query.url;
+
+        const response = await axios.get(apiUrl, {
+            withCredentials: false, params: {
+                translatedLanguage: req.query.translatedLanguage,
+            },
+        });
+
+        res.json(response.data);
+    } catch (error) {
+        console.error('API Proxy Error:', error.message);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+// Image proxy route
+app.use('/image', async (req, res) => {
+    try {
+        const imageUrl = req.query.url;
+        const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+        const contentType = response.headers['content-type'];
+        res.set('Content-Type', contentType);
+        res.send(response.data);
+    } catch (error) {
+        console.error('Image Proxy Error:', error.message);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Proxy Server is running on PORT:${port}`);
+});
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const axios = require('axios');
-const cors = require('cors'); // Import cors module
+const cors = require('cors');
+const compression = require('compression');
+const cache = require('memory-cache'); // Install using npm install memory-cache
 
 const app = express();
 const port = process.env.PORT || 4000;
-
 
 const allowedOrigins = [
     'https://manga-website1.netlify.app',
@@ -14,76 +15,115 @@ const allowedOrigins = [
 app.use(cors({
     origin: allowedOrigins,
 }));
-
-
+app.use(compression()); // Enable compression
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // API proxy route
-
 app.get('/api', async (req, res) => {
     try {
         const apiUrl = req.query.url;
+        const cachedData = cache.get(apiUrl);
 
-        const response = await axios.get(apiUrl, { withCredentials: false, });
+        if (cachedData) {
+            // Serve cached data
+            res.json(cachedData);
+        } else {
+            // Fetch data
+            const response = await axios.get(apiUrl, { withCredentials: false });
 
-        res.json(response.data);
+            // Cache data
+            cache.put(apiUrl, response.data, 60000); // Cache for 1 minute
+
+            res.json(response.data);
+        }
     } catch (error) {
         console.error('API Proxy Error:', error.message);
         res.status(500).send('Internal Server Error');
     }
 });
 
+// Manga proxy route
 app.get('/manga', async (req, res) => {
     try {
         const apiUrl = req.query.url;
+        const cachedData = cache.get(apiUrl);
 
-        const response = await axios.get(apiUrl, { withCredentials: false, params: { title: req.query.title } });
+        if (cachedData) {
+            // Serve cached data
+            res.json(cachedData);
+        } else {
+            // Fetch data
+            const response = await axios.get(apiUrl, {
+                withCredentials: false,
+                params: { title: req.query.title },
+            });
 
-        res.json(response.data);
+            // Cache data
+            cache.put(apiUrl, response.data, 60000); // Cache for 1 minute
+
+            res.json(response.data);
+        }
     } catch (error) {
         console.error('API Proxy Error:', error.message);
         res.status(500).send('Internal Server Error');
     }
 });
 
-
+// Mangas proxy route
 app.get('/mangas', async (req, res) => {
     try {
         const apiUrl = req.query.url;
+        const cachedData = cache.get(apiUrl);
 
-        console.log(req.query.url)
-
-        const response = await axios.get(apiUrl,
-            {
+        if (cachedData) {
+            // Serve cached data
+            res.json(cachedData);
+        } else {
+            // Fetch data
+            const response = await axios.get(apiUrl, {
                 withCredentials: false,
                 params: {
                     includedTags: req.query.includedTags,
                     excludedTags: req.query.excludedTags,
                     order: req.query.order,
-                    limit: req.query.limit
+                    limit: req.query.limit,
                 },
-                maxContentLength: Infinity,
             });
-        res.json(response.data);
+
+            // Cache data
+            cache.put(apiUrl, response.data, 60000); // Cache for 1 minute
+
+            res.json(response.data);
+        }
     } catch (error) {
         console.error('API Proxy Error:', error.message);
         res.status(500).send('Internal Server Error');
     }
 });
 
+// Chapters proxy route
 app.get('/chapters', async (req, res) => {
     try {
         const apiUrl = req.query.url;
+        const cachedData = cache.get(apiUrl);
 
-        const response = await axios.get(apiUrl, {
-            withCredentials: false, params: {
-                translatedLanguage: req.query.translatedLanguage,
-            },
-        });
+        if (cachedData) {
+            // Serve cached data
+            res.json(cachedData);
+        } else {
+            // Fetch data
+            const response = await axios.get(apiUrl, {
+                withCredentials: false,
+                params: { translatedLanguage: req.query.translatedLanguage },
+            });
 
-        res.json(response.data);
+            // Cache data
+            cache.put(apiUrl, response.data, 60000); // Cache for 1 minute
+
+            res.json(response.data);
+        }
     } catch (error) {
         console.error('API Proxy Error:', error.message);
         res.status(500).send('Internal Server Error');
@@ -94,10 +134,23 @@ app.get('/chapters', async (req, res) => {
 app.use('/image', async (req, res) => {
     try {
         const imageUrl = req.query.url;
-        const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
-        const contentType = response.headers['content-type'];
-        res.set('Content-Type', contentType);
-        res.send(response.data);
+        const cachedImage = cache.get(imageUrl);
+
+        if (cachedImage) {
+            // Serve cached image
+            res.set('Content-Type', cachedImage.contentType);
+            res.send(cachedImage.data);
+        } else {
+            // Fetch image
+            const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+            const contentType = response.headers['content-type'];
+
+            // Cache image
+            cache.put(imageUrl, { contentType, data: response.data }, 60000); // Cache for 1 minute
+
+            res.set('Content-Type', contentType);
+            res.send(response.data);
+        }
     } catch (error) {
         console.error('Image Proxy Error:', error.message);
         res.status(500).send('Internal Server Error');
